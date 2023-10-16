d9a94cf8a2ca2122528fe34ce1a0d0efafdf1001
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2023-10-16 10:14:27 -0400

    1

---
+++ dom.js
@@ -0,0 +1,14 @@
+const DOMSelectors = {
+    form:document.querySelector("#form"),
+    first:document.querySelector(".first-name"),
+    h2s:document.querySelectorAll("h2"),
+    //select the textbox
+    //select ALL the h2s in one property
+};
+
+DOMSelectors.form.addEventListener("submit",function(event) {
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value
+    );
+});
