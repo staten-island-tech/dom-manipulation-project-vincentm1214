c3264a4887fbd27868cb4ffe4ddf08e73fea2388
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2023-10-27 10:13:21 -0400

    7

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-// const form=document.querySelector("#form")
-// console.log
+const form=document.querySelector("#form")
+console.log
 
-// const DOMSelectors = {
-//     form:document.querySelector("#form"),
-//     first:document.querySelector(".full-name"),
-//     // h2s:document.querySelectorAll("h2"),
-//     question:document.querySelector(".question"),
-//     link:document.querySelector(".link")
-//     //select the textbox
-//     //select ALL the h2s in one property
+const DOMSelectors = {
+    form:document.querySelector("#form"),
+    first:document.querySelector(".full-name"),
+    // h2s:document.querySelectorAll("h2"),
+    question:document.querySelector(".question"),
+    link:document.querySelector(".link")
+    //select the textbox
+    //select ALL the h2s in one property
 
-// };
+};
 
-// const name = {
-    
-// }
-
-// function name(){
-
-// }
-
-
-
-
-// DOMSelectors.form.addEventListener("submit",function(event) {
-//     event.preventDefault();
-//     console.log(DOMSelectors.firstName.value);
-//     DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value
-//     );
-//     Document
-//     .querySelector(".container")
-//     .insertAdjacentHTML(
-//         "afterbegin",
-//         <div class="card"><h2 class="card-title"><div class="card-img"></div></h2></div>
-//     );
-// });
+DOMSelectors.form.addEventListener("submit",function(event) {
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+     // DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value
+    // );
+    Document
+    .querySelector(".container")
+    .insertAdjacentHTML(
+        "afterbegin",
+        <div class="card"><h2 class="card-title"><div class="card-img"></div></h2></div>
+    );
+});
 
